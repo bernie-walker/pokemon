c6343daca37ab00f10e94ec439254ebc19ed7977
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-01-09 14:19:43 +0530

    modified automator and added all the pokemons to the page

--- automator.js
+++ automator.js
@@ -1,34 +1,16 @@
-const reference = {
-  _id: '557a723880a20c9db3bc31c2',
-  pkdx_id: 1,
-  national_id: 1,
-  name: 'Bulbasaur',
-  __v: 3,
-  image_url: 'http://pokeapi.co/media/img/1.png',
-  description:
-    "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.  Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.",
-  art_url: 'http://assets22.pokemon.com/assets/cms2/img/pokedex/full/001.png',
-  types: ['poison', 'grass'],
-  evolutions: [
-    {
-      level: 16,
-      method: 'level_up',
-      to: 'Ivysaur',
-      _id: '557a723880a20c9db3bc31c3'
-    }
-  ]
-};
+const pokemonList = require('./pokemon.json');
 
-const pokeName = reference.name;
-const imageSource = reference.art_url;
-const abilities = reference.types
-  .map(ab => {
-    const name = ab[0].toUpperCase() + ab.slice(1);
-    return `<p class="${ab}">${name}</p>`;
-  })
-  .join('\n');
+const getDiv = function(reference) {
+  const pokeName = reference.name;
+  const imageSource = reference.art_url;
+  const abilities = reference.types
+    .map(ab => {
+      const name = ab[0].toUpperCase() + ab.slice(1);
+      return `<p class="${ab}">${name}</p>`;
+    })
+    .join('\n');
 
-const div = `<button class="poke-card">
+  const div = `<button class="poke-card">
 <h2 class="poke-name">${pokeName}</h2>
 <img
   class="poke-image"
@@ -39,4 +21,7 @@ const div = `<button class="poke-card">
 </div>
 </button>`;
 
-console.log(div);
+  return div;
+};
+
+console.log(pokemonList.map(getDiv).join('\n\n'));
